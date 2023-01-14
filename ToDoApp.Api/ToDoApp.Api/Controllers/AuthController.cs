41c1b7eee7656e8a4d4115cc3220044e74ad303b
bukhuti-vardanidze <84092360+bukhuti-vardanidze@users.noreply.github.com>
2023-01-14 15:07:00 +0400

    practice

---
+++ ToDoApp.Api/ToDoApp.Api/Controllers/AuthController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoApp.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
+    {
+        [HttpGet]
+        [Route( "ping")]
+        public string Ping()
+        {
+            return "pong";
+        }
+
+
+    }
+}
