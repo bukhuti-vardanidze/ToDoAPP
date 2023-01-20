ca940a5c40933f4d47c7e08b304a91d254d8505f
bukhuti-vardanidze <84092360+bukhuti-vardanidze@users.noreply.github.com>
2023-01-20 14:06:16 +0400

    Create SendEmailRequestRepository.cs

---
+++ ToDoApp.Api/ToDoApp.Api/Models/Repositories/SendEmailRequestRepository.cs
@@ -0,0 +1,19 @@
+﻿using ToDoApp.Api.Db;
+
+namespace ToDoApp.Api.Models.Repositories
+{
+    public interface ISendEmailRequestRepository
+    {
+        //....
+    }
+
+    public class SendEmailRequestRepository : ISendEmailRequestRepository
+    {
+
+        private readonly AppDbContext _db;
+
+        //..
+    }
+
+    
+}
